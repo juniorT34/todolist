70975c9c1b49226fccbd73d38c768638ef540278
juniorT34 <jj1982268@gmail.com>
2025-12-16 17:33:57 +0200

    Add .env file for environment variable management and update database connection to use DATABASE_URL

--- utils/database.py
+++ utils/database.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:junior@localhost:5432/miniproject"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
